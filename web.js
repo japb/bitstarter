33d8ebe04a40836ebcdc5e29f4dc6e541b44952f
James Baril <jbaril@gmail.com>
2013-07-04 17:22:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
+var buffer  = new Buffer(fs.readFileSync('index.html'));
+//console.log(buffer.toString());
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
+  //response.send('Hello World 2!');
+//var buffer  = new Buffer((fs.readFilesync('index.html'));
+//console.log(buffer);
+//fs.readFileSync('index.html')
+//buf.toString([encoding], [start], [end])
 });
 
 var port = process.env.PORT || 5000;
